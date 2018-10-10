4d4e24ea4ce0b76e0520b5794ba2d81e457f2346
Jose Garcia Domene <jose.garcia.domene@icloud.com>
2018-10-11 00:36:31 +0200

    VCT 3.0

--- create_app.sh
+++ create_app.sh
@@ -2,7 +2,7 @@ VERSION=$1
 hdiutil detach "/Volumes/Installer"
 hdiutil detach "/Volumes/VCT${VERSION} Installer"
 rm -rf __pycache__ build dist *.spec VCT*.dmg
-pyinstaller --onefile --windowed --add-binary bin/ffmpeg:bin --add-binary bin/ffprobe:bin --add-binary bin/ffplay:bin --icon vct.icns vct.py
+pyinstaller --onefile --windowed --add-binary bin/ffmpeg:bin --add-binary bin/ffprobe:bin --icon vct.icns vct.py
 cp ./Installer.dmg.disk tmp.dmg
 hdiutil attach ./tmp.dmg
 cp -R ./dist/vct.app /Volumes/Installer
