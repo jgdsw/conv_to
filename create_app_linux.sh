3c5aaab8c74e35e03b2998e848d4f1e03b979378
Jose Garcia Domene <jose.garcia.domene@icloud.com>
2018-10-20 22:29:08 +0200

    VCT v3.0.2 Final Release - Linux support

--- create_app_linux.sh
+++ create_app_linux.sh
@@ -3,5 +3,5 @@ rm -rf __pycache__ build dist VCT*.dmg VCT.spec *.tar.gz
 pyinstaller --onefile --windowed --icon vct.png vct.py -n VCT
 mv ./dist/VCT .
 rm -rf __pycache__ build dist VCT*.dmg VCT.spec
-tar cvzf VCTv${VERSION}-linux-x64.tar.gz VCT
+tar cvzf VCTv${VERSION}-linux-x86_64.tar.gz VCT
 rm VCT
